932bd1e03400728c2e5744159c7be23026b87a23
s.hajiyan <s.hajiyan@DESKTOP-HC87PON>
2023-11-15 10:55:43 +0330

    Domain Layer

---
+++ Src/Core/Base.Core.Domain/Base.Core.Domain/Entities/IAggregateRoot.cs
@@ -0,0 +1,15 @@
+﻿using Base.Core.Domain.Event;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Core.Domain.Entities
+{
+    public interface IAggregateRoot
+    {
+        void ClearEvents();
+        IEnumerable<IDomainEvent> GetEvents();
+    }
+}
---
+++ Src/Core/Base.Core.Domain/Base.Core.Domain/ValueObject/UniqId.cs
@@ -0,0 +1,59 @@
+﻿using Base.Core.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Base.Core.Domain.ValueObject.UniqId;
+
+namespace Base.Core.Domain.ValueObject
+{
+    public class UniqId : BaseValueObject<UniqId>
+    {
+
+  
+        public static UniqId FromString(string value) => new(value);
+        public static UniqId FromGuid(Guid value) => new() { Value = value };
+
+        public UniqId(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new InvalidValueObjectStateException("ValidationErrorIsRequire", nameof(UniqId));
+            }
+            if (Guid.TryParse(value, out Guid tempValue))
+            {
+                Value = tempValue;
+            }
+            else
+            {
+                throw new InvalidValueObjectStateException("ValidationErrorInvalidValue", nameof(UniqId));
+            }
+        }
+        private UniqId()
+        {
+
+        }
+
+        public Guid Value { get; private set; }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+
+        public static explicit operator string(UniqId title) => title.Value.ToString();
+        public static implicit operator UniqId(string value) => new(value);
+
+
+        public static explicit operator Guid(UniqId title) => title.Value;
+        public static implicit operator UniqId(Guid value) => new() { Value = value };
+
+    }
+
+}
