765204423d1a86d554286bc1b4fe00acd32448b7
s.hajiyan <s.hajiyan@DESKTOP-HC87PON>
2023-11-18 10:51:16 +0330

    Create Sample First

---
+++ Sample/Core/Sample.Core.Domain/Person/Entities/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Core.Domain.Person.Entities
+{
+    public class Person : AggregateRoot<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Sample/Core/Sample.Core.Domain/Person/ValueObject/FirstName.cs
@@ -0,0 +1,20 @@
+﻿using Base.Core.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Core.Domain.Person.ValueObject
+{
+    public record FirstName 
+    {
+        public string Value { get; init; }
+        public FirstName()
+        {
+            if (string.IsNullOrEmpty(Value))
+                throw new InvalidValueObjectStateException()
+        }
+    }
+}
